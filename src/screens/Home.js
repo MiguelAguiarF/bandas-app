62be4fc0ea2a49efe5d8d95e6c78bec0f5f34e93
MiguelAguiarF <miguelaguiarf5@gmail.com>
2024-04-05 14:54:51 -0300

    2atualizacao

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
 import MusicItem from "../components/MusicItem";
 
 export default function Home({navigation}) {
   const [currentPlaying, setCurrentPlaying] = useState(null);
+  const [musicData, setMusicData] = useState([])
   const item ={
     id: 1,
     title:"Highway to Hell",
     group: "AC/DC",
     album_image: "https://upload.wikimedia.org/wikipedia/pt/a/ac/Acdc_Highway_to_Hell.JPG",
-  }
+  };
+
+  useEffect(() => {
+    fetch("http://10.0.2.2:3000/musics").then((response)=>response.json())
+    .then((data)=>setMusicData(data));
+  }, []);
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#121212"/>
       <Text style={styles.title}>Home</Text>
+      <FlatList data={musicData} keyExtractor={(item)=> item.id.toString()} renderItem={({item}) =>(
       <MusicItem isPlaying={() => currentPlaying == item.id} music={item} navigation={navigation} onPlayPause={() => {}} />
+      )}/>
     </View>
   );
 }
