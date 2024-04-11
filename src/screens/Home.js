e92132b198b32a5985326a8afc4f016502e1c553
MiguelAguiarF <miguelaguiarf5@gmail.com>
2024-04-11 11:09:43 -0300

    atualizacao3

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,45 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
 import MusicItem from "../components/MusicItem";
+import { Audio } from "expo-av";
 
-export default function Home({navigation}) {
+export default function Home({ navigation }) {
+  const [musicData, setMusicData] = useState([]);
   const [currentPlaying, setCurrentPlaying] = useState(null);
-  const [musicData, setMusicData] = useState([])
-  const item ={
-    id: 1,
-    title:"Highway to Hell",
-    group: "AC/DC",
-    album_image: "https://upload.wikimedia.org/wikipedia/pt/a/ac/Acdc_Highway_to_Hell.JPG",
+  const [currentSound, setCurrentSound] = useState(null);
+
+  const togglePlayPause = async (item) => {
+    if (currentSound && currentPlaying === item.id) {
+      await currentSound.pauseAsync();
+      setCurrentPlaying(null);
+      setCurrentSound(null);
+    } else {
+      if (currentSound) {
+        await currentSound.unloadAsync();
+      }
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: `http://10.0.2.2:3000/musics/${item.music_path}` },
+        { shouldPlay: true }
+      );
+      setCurrentSound(sound);
+      setCurrentPlaying(item.id);
+    }
   };
 
   useEffect(() => {
-    fetch("http://10.0.2.2:3000/musics").then((response)=>response.json())
-    .then((data)=>setMusicData(data));
+    fetch("http://10.0.2.2:3000/musics")
+      .then((response) => response.json())
+      .then((data) => setMusicData(data));
   }, []);
+
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#121212"/>
-      <Text style={styles.title}>Home</Text>
-      <FlatList data={musicData} keyExtractor={(item)=> item.id.toString()} renderItem={({item}) =>(
-      <MusicItem isPlaying={() => currentPlaying == item.id} music={item} navigation={navigation} onPlayPause={() => {}} />
-      )}/>
+      <StatusBar barStyle="light-content" backgroundColor="#121212" />
+      <Text style={styles.title}>Minhas Músicas</Text>
+
+
+      <FlatList
+        data={musicData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <MusicItem
+            music={item}
+            onPlayPause={() => togglePlayPause(item)}
+            isPlaying={currentPlaying === item.id}
+            navigation={navigation}
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: "#121212",
     paddingTop: 16,
   },
-
   title: {
     fontSize: 36,
     fontWeight: "bold",
     color: "#fff",
     marginBottom: 20,
     marginLeft: 20,
-  }
-})
+  },
+});
+
+
+
